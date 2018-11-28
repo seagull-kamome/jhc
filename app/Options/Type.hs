3062f23f4b13e67a18b846589a3dc1d2666b6a54
Hattori, Hiroki <seagull.kamome@gmail.com>
2018-11-28 12:27:19 +0000

    [master] :recycle: cleanup Option/Types.hs

--- app/Options/Type.hs
+++ app/Options/Type.hs
@@ -1,11 +1,10 @@
-{-# OPTIONS -w -funbox-strict-fields #-}
 module Options.Type(
     StopCondition(..),
     theoptions,Opt(..),
     Mode(..),
     helpUsage,
-    optFOptsSet_u,
-    prettyOptions,
+    -- optFOptsSet_u,
+    -- prettyOptions,
     emptyOpt,
     postProcessFD
     ,postProcessFO
@@ -15,58 +14,73 @@ import Data.List(intercalate)
 import System.Console.GetOpt
 
 import Options.Map
-import Util.DocLike
+-- import Util.DocLike
 import qualified Data.Map as M
 import qualified Data.Set as S
 import qualified FlagDump as FD
 import qualified FlagOpts as FO
 
+
+
 {-# NOINLINE theoptions #-}
 theoptions :: [OptDescr (Opt -> Opt)]
 theoptions =
-    [Option ['V'] ["version"]         (NoArg  (optMode_s Version))          "print version info and exit"
-    ,Option ['h'] ["help"]            (NoArg  (optMode_s ShowHelp))         "print help information and exit"
-    ,Option []    ["info"]            (NoArg  (optMode_s ShowConfig))       "show compiler configuration information and exit"
-    ,Option []    ["purge-cache"]     (NoArg  (optMode_s PurgeCache))       "clean out jhc compilation cache"
-    ,Option ['v'] ["verbose"]         (NoArg  (optVerbose_u (+1)))          "chatty output on stderr"
-    ,Option ['z'] []                  (NoArg  (optStatLevel_u (+1)))        "Increase verbosity of statistics"
-    ,Option ['d'] []                  (ReqArg (optDump_u . (:))  "[no-]flag") "dump specified data during compilation"
-    ,Option ['f'] []                  (ReqArg (optFOpts_u . (:)) "[no-]flag") "set or clear compilation options"
-    ,Option ['X'] []                  (ReqArg (optExtensions_u . (:))  "Extension") "enable the given language extension by the cabal name. see -fhelp for jhc native names."
-    ,Option ['o'] ["output"]          (ReqArg (optOutName_s . Just) "FILE") "output to FILE"
-    ,Option ['i'] ["include"]         (ReqArg (optIncdirs_u . idu) "DIR")   "path to look for haskell source files. use -i- to clear the search path."
-    ,Option ['I'] []                  (ReqArg (optIncs_u . idu) "DIR")       "add to preprocessor include path"
-    ,Option []    ["with"]            (ReqArg (optWith_u . idu) "foo.yaml") "include values from yaml file in configuration"
-    ,Option ['D'] []                  (ReqArg (optDefs_u . (:)) "NAME=VALUE") "add new definitions to set in preprocessor"
-    ,Option []    ["optc"]            (ReqArg (optCCargs_u . idu) "option") "extra options to pass to c compiler"
-    ,Option ['c'] []                  (NoArg  (optStop_s CompileHo))        "just compile the modules, caching the results."
-    ,Option ['C'] []                  (NoArg  (optStop_s StopC))            "compile to C code"
-    ,Option ['E'] []                  (NoArg  (optMode_s Preprocess))       "preprocess the input and print result to stdout"
-    ,Option ['k'] ["keepgoing"]       (NoArg  (optKeepGoing_s True))        "keep going on errors"
-    ,Option []    ["cross"]           (NoArg  (optCross_s True))            "enable cross-compilation, choose target with the -m flag"
-    ,Option []    ["stop"]            (ReqArg (optStop_s . stop) "parse/typecheck/c") "stop after the given pass, parse/typecheck/c"
-    ,Option []    ["width"]           (ReqArg (optColumns_s . read) "COLUMNS") "width of screen for debugging output"
-    ,Option []    ["main"]            (ReqArg (optMainFunc_s . Just . (,) False) "Main.main")  "main entry point"
-    ,Option ['m'] ["arch"]            (ReqArg (optArch_u . idu ) "arch")      "target architecture options"
-    ,Option []    ["entry"]           (ReqArg (optMainFunc_s . Just . (,) True)  "<expr>")  "main entry point, showable expression"
+    [Option ['V'] ["version"]         (NoArg  (\x -> x { optMode = Version }))          "print version info and exit"
+    ,Option ['h'] ["help"]            (NoArg  (\x -> x { optMode = ShowHelp }))         "print help information and exit"
+    ,Option []    ["info"]            (NoArg  (\x -> x { optMode = ShowConfig }))       "show compiler configuration information and exit"
+    ,Option []    ["purge-cache"]     (NoArg  (\x -> x { optMode = PurgeCache }))       "clean out jhc compilation cache"
+    ,Option ['v'] ["verbose"]         (NoArg  (\x -> x { optVerbose = optVerbose x + 1}))          "chatty output on stderr"
+    ,Option ['z'] []                  (NoArg  (\x -> x { optStatLevel = optStatLevel x + 1 }))        "Increase verbosity of statistics"
+    ,Option ['d'] []                  (ReqArg (\y x -> x { optDump = y:optDump x }) "[no-]flag") "dump specified data during compilation"
+    ,Option ['f'] []                  (ReqArg (\y x -> x { optFOpts = y:optFOpts x }) "[no-]flag") "set or clear compilation options"
+    ,Option ['X'] []                  (ReqArg (\y x -> x { optExtensions = y:optExtensions x })  "Extension") "enable the given language extension by the cabal name. see -fhelp for jhc native names."
+    ,Option ['o'] ["output"]          (ReqArg (\y x -> x { optOutName = Just y })  "FILE") "output to FILE"
+    ,Option ['i'] ["include"]         (ReqArg (\y x -> x { optIncdirs = idu y $ optIncdirs x }) "DIR")   "path to look for haskell source files. use -i- to clear the search path."
+    ,Option ['I'] []                  (ReqArg (\y x -> x { optIncs = idu y $ optIncs x }) "DIR")       "add to preprocessor include path"
+    ,Option []    ["with"]            (ReqArg (\y x -> x { optWith = idu y $ optWith x }) "foo.yaml") "include values from yaml file in configuration"
+    ,Option ['D'] []                  (ReqArg (\y x -> x { optDefs = y:optDefs x }) "NAME=VALUE") "add new definitions to set in preprocessor"
+    ,Option []    ["optc"]            (ReqArg (\y x -> x { optCCargs = idu y $ optCCargs x }) "option") "extra options to pass to c compiler"
+    ,Option ['c'] []                  (NoArg  (\x -> x { optStop = CompileHo }))        "just compile the modules, caching the results."
+    ,Option ['C'] []                  (NoArg  (\x -> x { optStop = StopC }))            "compile to C code"
+    ,Option ['E'] []                  (NoArg  (\x -> x { optMode = Preprocess }))       "preprocess the input and print result to stdout"
+    ,Option ['k'] ["keepgoing"]       (NoArg  (\x -> x { optKeepGoing = True }))        "keep going on errors"
+    ,Option []    ["cross"]           (NoArg  (\x -> x { optCross =  True }))            "enable cross-compilation, choose target with the -m flag"
+    ,Option []    ["stop"]            (ReqArg (\y x -> x { optStop = stop y }) "parse/typecheck/c") "stop after the given pass, parse/typecheck/c"
+    ,Option []    ["width"]           (ReqArg (\y x -> x { optColumns = read y})  "COLUMNS") "width of screen for debugging output"
+    ,Option []    ["main"]            (ReqArg (\y x -> x { optMainFunc = Just (False, y) }) "Main.main")  "main entry point"
+    ,Option ['m'] ["arch"]            (ReqArg (\y x -> x { optArch = idu y $ optArch x }) "arch")      "target architecture options"
+    ,Option []    ["entry"]           (ReqArg (\y x -> x { optMainFunc = Just (True, y) })  "<expr>")  "main entry point, showable expression"
 --  ,Option ['e'] []                  (ReqArg (\d -> optStmts_u ( d:)) "<statement>")  "run given statement as if on jhci prompt"
-    ,Option []    ["show-ho"]         (ReqArg (optMode_s . ShowHo) "file.ho") "Show contents of ho or hl file. Use -dflag to determine what information to show."
-    ,Option []    ["noauto"]          (NoArg  (optNoAuto_s True))           "Do not automatically depend on primitive libraries. Used to build jhc-prim. Use -p- to clear the visible package list."
-    ,Option ['p'] ["package"]         (ReqArg (optHls_u . (:)) "package")   "Load given haskell library package. Use -p- to remove all current entries from the list."
-    ,Option ['L'] []                  (ReqArg (optHlPath_u . idu) "path")   "Look for haskell libraries in the given directory. Use -L- to clear the search path."
-    ,Option []    ["build-hl"]        (ReqArg (optMode_s . BuildHl) "desc.yaml") "Build hakell library from given library description file"
-    ,Option []    ["annotate-source"] (ReqArg (optAnnotate_s . Just) "<dir>") "Write preprocessed and annotated source code to the directory specified"
-    ,Option []    ["deps"]            (ReqArg (optDeps_s . Just) "<file.yaml>") "Write dependency information to file specified"
-    ,Option []    ["interactive"]     (NoArg  (optMode_s Interactive))      "run interactivly ( for debugging only)"
-    ,Option []    ["ignore-cache"]    (NoArg  (optIgnoreHo_s True))         "Ignore existing compilation cache entries."
-    ,Option []    ["readonly-cache"]  (NoArg  (optNoWriteHo_s True))        "Do not write new information to the compilation cache."
-    ,Option []    ["no-cache"]        (NoArg  (optNoWriteHo_s True . optIgnoreHo_s True)) "Do not use or update the cache."
-    ,Option []    ["cache-dir"]       (ReqArg (optHoCache_s . Just ) "JHC_CACHE")  "Use a global cache located in the directory passed as an argument."
-    ,Option []    ["stale"]           (ReqArg (optStale_u . idu) "Module")  "Treat these modules as stale, even if they exist in the cache."
-    ,Option []    ["list-libraries"]  (NoArg  (optMode_s ListLibraries))    "List of installed libraries."
-    ,Option []    ["tdir"]            (ReqArg (optWorkDir_s . Just) "dir/") "specify the directory where all intermediate files/dumps will be placed."
+    ,Option []    ["show-ho"]         (ReqArg (\y x -> x { optMode = ShowHo y }) "file.ho") "Show contents of ho or hl file. Use -dflag to determine what information to show."
+    ,Option []    ["noauto"]          (NoArg  (\x -> x { optNoAuto =  True }))           "Do not automatically depend on primitive libraries. Used to build jhc-prim. Use -p- to clear the visible package list."
+    ,Option ['p'] ["package"]         (ReqArg (\y x -> x { optHls = y:optHls x }) "package")   "Load given haskell library package. Use -p- to remove all current entries from the list."
+    ,Option ['L'] []                  (ReqArg (\y x -> x { optHlPath = idu y $ optHlPath x }) "path")   "Look for haskell libraries in the given directory. Use -L- to clear the search path."
+    ,Option []    ["build-hl"]        (ReqArg (\y x -> x { optMode = BuildHl y }) "desc.yaml") "Build hakell library from given library description file"
+    ,Option []    ["annotate-source"] (ReqArg (\y x -> x { optAnnotate = Just y }) "<dir>") "Write preprocessed and annotated source code to the directory specified"
+    ,Option []    ["deps"]            (ReqArg (\y x -> x { optDeps = Just y }) "<file.yaml>") "Write dependency information to file specified"
+    ,Option []    ["interactive"]     (NoArg  (\x -> x { optMode = Interactive }))      "run interactivly ( for debugging only)"
+    ,Option []    ["ignore-cache"]    (NoArg  (\x -> x { optIgnoreHo = True }))         "Ignore existing compilation cache entries."
+    ,Option []    ["readonly-cache"]  (NoArg  (\x -> x { optNoWriteHo = True }))        "Do not write new information to the compilation cache."
+    ,Option []    ["no-cache"]        (NoArg  (\x -> x { optNoWriteHo = True, optIgnoreHo = True })) "Do not use or update the cache."
+    ,Option []    ["cache-dir"]       (ReqArg (\y x -> x { optHoCache = Just y }) "JHC_CACHE")  "Use a global cache located in the directory passed as an argument."
+    ,Option []    ["stale"]           (ReqArg (\y x  -> x { optStale = idu y $ optStale x }) "Module")  "Treat these modules as stale, even if they exist in the cache."
+    ,Option []    ["list-libraries"]  (NoArg  (\x -> x { optMode = ListLibraries }))    "List of installed libraries."
+    ,Option []    ["tdir"]            (ReqArg (\y x -> x { optWorkDir = Just y }) "dir/") "specify the directory where all intermediate files/dumps will be placed."
 --   , Option []    ["print-hsc-options"] (NoArg (optMode_s PrintHscOptions)) "print options to pass to hsc2hs"
     ]
+  where
+    idu :: String -> [String] -> [String]
+    idu "-" _ = []
+    idu d ds = ds ++ [d]
+    stop :: String ->  StopCondition
+    stop "parse" = StopParse
+    stop "deps" = StopParse
+    stop "typecheck" = StopTypeCheck
+    stop "c" = StopC
+    stop s = StopError s
+
+
+
 
 data Mode
     = BuildHl FilePath         -- ^ Build the specified hl-file given a description file.
@@ -128,7 +142,7 @@ data Opt = Opt {
     optInis        ::  M.Map String String,    -- ^ options read from ini files
     optDumpSet     ::  S.Set FD.Flag,    -- ^ Dump flags.
     optFOptsSet    ::  S.Set FO.Flag     -- ^ Flag options (-f\<opt\>).
-  } {-!derive: update !-}
+  }
 
 emptyOpt = Opt {
     optMode        = CompileExe,
@@ -168,14 +182,6 @@ emptyOpt = Opt {
     optFOptsSet    = languageDefault
 }
 
-idu "-" _ = []
-idu d ds = ds ++ [d]
-
-stop "parse" = StopParse
-stop "deps" = StopParse
-stop "typecheck" = StopTypeCheck
-stop "c" = StopC
-stop s = StopError s
 
 {-# NOINLINE fileOptions #-}
 fileOptions :: Monad m => Opt -> [String] -> m Opt
@@ -200,7 +206,7 @@ postProcessFO o = case FO.process (optFOptsSet o) (optFOpts o) of
         (_,xs) -> fail ("Unrecognized flag passed to '-f': "
                         ++ unwords xs ++ "\nValid flags:\n\n" ++ FO.helpMsg)
 
-{-# NOINLINE prettyOptions #-}
+#if 0
 prettyOptions :: String
 prettyOptions =  showSD $ vcat ([h1 "Usage Examples"] ++ usages
         ++ [h1 "Option Flags"] ++ flags) where
@@ -228,24 +234,27 @@ prettyOptions =  showSD $ vcat ([h1 "Usage Examples"] ++ usages
 
     header = pads ["flag","arg","description"]
     pads xs = hsep [ pad p x | (x,p) <- zip xs colnums ]
-    pad n t = text $ t ++ replicate (n - length t) ' '
+    pad :: Int -> String -> String
+    pad n t = t ++ replicate (n - length t) ' '
     len sh = length $ show sh
-
-pfill ::
-    Int            -- ^ maximum width
-    -> (a -> Int)  -- ^ find width of any element
-    -> [a]         -- ^ input elements
-    -> [[a]]       -- ^ output element
-pfill maxn length xs = f maxn xs [] [] where
-    f n (x:xs) ws ls | lx < n = f (n - lx) xs (x:ws) ls where
-        lx = length x
-    f _ (x:xs) [] ls = f (maxn - length x) xs [x] ls
-    f _ (x:xs) ws ls = f (maxn - length x) xs [x] (ws:ls)
-    f _ [] [] ls = reverse (map reverse ls)
-    f _ [] ws ls = reverse (map reverse (ws:ls))
+#endif
 
 helpUsage :: String
 helpUsage = usageInfo header theoptions ++ trailer where
     header = "Usage: jhc [OPTION...] Main.hs"
     trailer = "\n" ++ mkoptlist "-d" FD.helpFlags ++ "\n" ++ mkoptlist "-f" FO.helpFlags
     mkoptlist d os = "valid " ++ d ++ " arguments: 'help' for more info\n    " ++ intercalate "\n    " (map (intercalate ", ") $ pfill 80 ((2 +) . length) os) ++ "\n"
+    pfill ::
+        Int            -- ^ maximum width
+        -> (a -> Int)  -- ^ find width of any element
+        -> [a]         -- ^ input elements
+        -> [[a]]       -- ^ output element
+    pfill maxn length xs = f maxn xs [] [] where
+        f n (x:xs) ws ls | lx < n = f (n - lx) xs (x:ws) ls where
+            lx = length x
+        f _ (x:xs) [] ls = f (maxn - length x) xs [x] ls
+        f _ (x:xs) ws ls = f (maxn - length x) xs [x] (ws:ls)
+        f _ [] [] ls = reverse (map reverse ls)
+        f _ [] ws ls = reverse (map reverse (ws:ls))
+
+
