451f1a5618fad862ecf44b4daf0c08ca071d5feb
Hattori, Hiroki <seagull.kamome@gmail.com>
2018-12-14 10:29:21 +0000

    [master] :recycle: cleanup

--- app/Name/Id.hs
+++ app/Name/Id.hs
@@ -31,32 +31,25 @@ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 -}
 module Name.Id (
-  Name, AnyName(..), KindedName(..),
-  nameIdKind, nameKindSing, toUnknownName, toName,
-  forgeName, nameParts,
+  IdKinds(..),
   --
-  Id, AnyId(..), KindedId(..),
-  IdSet, IdMap, AnyIdSet, AnyIdMap, KindedIdSet, KindedIdMap,
-  KnownIdKind(..),
-  idKindSing, toUnknownId, toId,
-  FreshIdT, MonadFreshId(..), runFreshIdT
+  ModuleName(..), toModuleName,
+  --
+  Name(..), toName, forgeName, nameParts,
+  --
+  Id(..), IdSet, IdMap,
+  idKind
   ) where
 
-import Control.Monad.Trans
-import Control.Monad.State.Strict
 import Data.Set as SET
 import Data.Map.Strict as MAP
--- import GHC.TypeLits
--- import GHC.Exts (IsString(..))
 import GHC.Generics
--- import Data.Proxy
 
 import qualified Data.Text as T
-import Data.Interned (intern)
+import Data.Interned (intern, unintern)
 import Data.Interned.Text
 import qualified Data.Binary as BIN
 
--- import Control.Monad.Fresh
 
 -- ---------------------------------------------------------------------------
 
@@ -67,176 +60,77 @@ data IdKinds
   | ModuleName | ClassName | SortName | FieldLabel
   | RawType
   | QuotedName
-  deriving (Show, Generic)
+  deriving (Enum, Eq, Show, Generic)
 instance BIN.Binary IdKinds
 
-class KnownIdKind (k :: IdKinds) where toIdKind :: proxy k -> IdKinds
-instance KnownIdKind 'TypeConstructor where toIdKind _ = TypeConstructor
-instance KnownIdKind 'DataConstructor where toIdKind _ = DataConstructor
-instance KnownIdKind 'TypeVal where toIdKind _ = TypeVal
-instance KnownIdKind 'TermVal where toIdKind _ = TermVal
-instance KnownIdKind 'ModuleName where toIdKind _ = ModuleName
-instance KnownIdKind 'ClassName where toIdKind _ = ClassName
-instance KnownIdKind 'SortName where toIdKind _ = SortName
-instance KnownIdKind 'FieldLabel where toIdKind _ = FieldLabel
-instance KnownIdKind 'RawType where toIdKind _ = RawType
-instance KnownIdKind 'QuotedName where toIdKind _ = QuotedName
+-- ---------------------------------------------------------------------------
+
+data ModuleName
+  = UnqualifiedModuleName { moduleBaseName :: !InternedText }
+  | QualifiedModuleName { moduleParentName :: !ModuleName
+                        , moduleBaseName :: !InternedText }
+  deriving (Show, Eq)
+instance BIN.Binary ModuleName where
+  put (UnqualifiedModuleName x) = BIN.putWord8 0 >> BIN.put (unintern x)
+  put (QualifiedModuleName x y) = BIN.putWord8 1 >> BIN.put x >> BIN.put (unintern y)
+  get = BIN.getWord8 >>= \case
+    0 -> UnqualifiedModuleName <$> (intern <$> BIN.get)
+    1 -> QualifiedModuleName <$> BIN.get <*> (intern <$> BIN.get)
+    _ -> undefined
+
+toModuleName :: Maybe ModuleName -> T.Text -> ModuleName
+toModuleName Nothing = UnqualifiedModuleName . intern
+toModuleName (Just x) = QualifiedModuleName x . intern
 
 
-data SingK (k :: IdKinds) where
-  SingUnknown :: SingK 'Unknown
-  SingTypeConstructor :: SingK 'TypeConstructor
-  SingDataConstructor :: SingK 'DataConstructor
-  SingTypeVal :: SingK 'TypeVal
-  SingTermVal :: SingK 'TermVal
-  SingModuleName :: SingK 'ModuleName
-  SingClassName :: SingK 'ClassName
-  SingSortName :: SingK 'SortName
-  SingFieldLabel :: SingK 'FieldLabel
-  SingRawType :: SingK 'RawType
-  SingQuotedName :: SingK 'QuotedName
-deriving instance Show (SingK k)
-deriving instance Eq (SingK k)
+
+data Name = UnqualifiedName { nameKind:: !IdKinds, nameBase:: !InternedText }
+          | QualifiedName { nameKind:: !IdKinds
+                          , nameModule:: !ModuleName
+                          , nameBase :: !InternedText }
+  deriving (Show, Eq)
+instance BIN.Binary Name where
+  put (UnqualifiedName x y) = BIN.putWord8 0 >> BIN.put x >> BIN.put (unintern y)
+  put (QualifiedName x y z) = BIN.putWord8 1 >> BIN.put x >> BIN.put y >> BIN.put (unintern z)
+  get = BIN.getWord8 >>= \case
+    0 -> UnqualifiedName <$> BIN.get <*> (intern <$> BIN.get)
+    1 -> QualifiedName <$> BIN.get <*> BIN.get <*> (intern <$> BIN.get)
+    _ -> undefined
+
+toName :: Maybe ModuleName -> IdKinds ->T.Text -> Name
+toName Nothing k = UnqualifiedName k . intern
+toName (Just x) k = QualifiedName k x . intern
+
+forgeName :: ModuleName -> IdKinds -> T.Text -> Name
+forgeName m k = QualifiedName k m . intern
+
+
+nameParts :: Name -> (IdKinds, Maybe ModuleName, Name)
+nameParts x@(UnqualifiedName k _) = (k, Nothing, x)
+nameParts (QualifiedName k m b) = (k, Just m, UnqualifiedName k b)
+
 
 
 -- ---------------------------------------------------------------------------
 
-data Name (k :: IdKinds) where
-  UnqualifiedName' :: !InternedText -> Name 'Unknown
-  UnqualifiedName :: KnownIdKind k => !(SingK k) -> !InternedText -> Name k
-  QualifiedName' :: Name 'ModuleName -> !InternedText -> Name 'Unknown
-  QualifiedName :: KnownIdKind k => !(SingK k) -> !(Name 'ModuleName) -> !InternedText -> Name k
-deriving instance Show (Name k)
-deriving instance Eq (Name k)
-
-newtype AnyName = AnyName { fromAnyName :: forall k. Name k }
-deriving instance Show AnyName
-deriving instance Eq AnyName
-
-newtype KindedName = KindedName { fromKindedName :: forall k. KnownIdKind k => Name k }
--- deriving instance Show KindedName
--- deriving instance Eq KindedName
-
-nameIdKind :: Name (k :: IdKinds) -> IdKinds
-nameIdKind (UnqualifiedName' _) = Unknown
-nameIdKind (UnqualifiedName sing _) = toIdKind sing
-nameIdKind (QualifiedName' _ _) = Unknown
-nameIdKind (QualifiedName sing _ _) = toIdKind sing
+data Id = NameId Name | Id !IdKinds !Int
+  deriving (Show, Eq)
+instance BIN.Binary Id where
+  put (NameId x) = BIN.putWord8 0 >> BIN.put x
+  put (Id x y) = BIN.putWord8 1 >> BIN.put x >> BIN.put y
+  get = BIN.getWord8 >>= \case
+    0 -> NameId <$> BIN.get
+    1 -> Id <$> BIN.get <*> BIN.get
+    _ -> undefined
 
 
-nameKindSing :: Name (k :: IdKinds) -> SingK k
-nameKindSing (UnqualifiedName' _) = SingUnknown
-nameKindSing (UnqualifiedName sing _) = sing
-nameKindSing (QualifiedName' _ _) = SingUnknown
-nameKindSing (QualifiedName sing _ _) = sing
+type IdSet = SET.Set Id
+type IdMap a = MAP.Map Id a
 
 
-toUnknownName :: T.Text -> Name 'Unknown
-toUnknownName = UnqualifiedName' . intern
-
-
-toName :: KnownIdKind k => SingK k ->T.Text -> Name k
-toName sing = UnqualifiedName sing . intern
-
-
-forgeName :: KnownIdKind k => SingK k -> Name 'ModuleName -> T.Text -> Name k
---forgeName SingUnknown Nothing = UnqualifiedName' . intern
---forgeName SingUnknown (Just x) = QualifiedName' x . intern
--- forgeName sing Nothing = UnqualifiedName sing . intern
-forgeName sing x = QualifiedName sing x . intern
-
-
-nameParts :: Name k -> (SingK k, Maybe (Name 'ModuleName), Name k)
-nameParts x@(UnqualifiedName' _) = (SingUnknown, Nothing, x)
-nameParts x@(UnqualifiedName sing _) = (sing, Nothing, x)
-nameParts (QualifiedName' m name) = (SingUnknown, Just m, UnqualifiedName' name)
-nameParts (QualifiedName sing m name) = (sing, Just m, UnqualifiedName sing name)
-
-
--- ---------------------------------------------------------------------------
-
-data Id (k :: IdKinds) where
-  AtomId ::  Name k -> Id k
-  Id' :: !Int -> Id 'Unknown
-  Id :: KnownIdKind k => !(SingK k) -> !Int -> Id k
-deriving instance Show (Id k)
-deriving instance Eq (Id k)
-
-newtype AnyId = AnyId { fromAnyId :: forall (k :: IdKinds). Id k }
--- deriving instance Show AnyId
-deriving instance Eq AnyId
-
-newtype KindedId = KindedId { fromKindedId :: forall (k :: IdKinds). KnownIdKind k => Id k }
--- deriving instance Show KindedId
--- deriving instance Eq KindedId
-
-type AnyIdSet = SET.Set AnyId
-type KindedIdSet = SET.Set KindedId
-type IdSet k = SET.Set (Id k)
-
-type AnyIdMap a = MAP.Map AnyId a
-type KindedIdMap a = MAP.Map KindedId a
-type IdMap k a = MAP.Map (Id k) a
-
-
-
--- ---------------------------------------------------------------------------
-#if 0
-instance Eq (Id (k :: IdKinds)) where
-  (AtomId' x) == (AtomId' y) = x == y
-  (AtomId' x) == (AtomId SingUnknown y) = x == y
-  (AtomId SingUnknown x) == (AtomId' y) = x == y
-  (AtomId _ x) == (AtomId _ y) = x == y
-  (Id' x) == (Id' y) = x == y
-  (Id' x) == (Id SingUnknown y) = x == y
-  (Id SingUnknown x) == (Id' y) = x == y
-  (Id _ x) == (Id _ y) = x == y
-  _ == _ = False
-#endif
-
-
--- ---------------------------------------------------------------------------
-
-idKindSing :: forall (k :: IdKinds). Id k  -> SingK k
--- idKindSing (AtomId' _) = SingUnknown
-idKindSing (AtomId name) = nameKindSing name
-idKindSing (Id' _) = SingUnknown
-idKindSing (Id sing _) = sing
-
-
-toUnknownId :: T.Text -> Id 'Unknown
-toUnknownId = AtomId . UnqualifiedName' . intern
-{-# INLINE toUnknownId #-}
-
-
-
-toId :: KnownIdKind k => SingK k -> T.Text -> Id k
-toId sing = AtomId . UnqualifiedName sing . intern
-{-# INLINE toId #-}
-
--- freshUnknownId :: MonadFresh Int m _=> m (Id Unknown)
--- freshId :: MonadFresh Int m => SingK k -> m (Id k)
-
-
--- ---------------------------------------------------------------------------
-
-newtype FreshIdT m a = FreshIdT (StateT Int m a)
-  deriving (Functor, Applicative, Monad)
-instance MonadTrans FreshIdT where lift = FreshIdT . lift
-instance MonadIO m => MonadIO (FreshIdT m) where liftIO = lift.liftIO
-
-class MonadFreshId m where
-  freshUnknonwId :: m (Id 'Unknown)
-  freshId :: KnownIdKind k => SingK k -> m (Id k)
-instance Monad m => MonadFreshId (FreshIdT m) where
-  freshUnknonwId = FreshIdT $ do { x <- get; put (x + 1); return $ Id' x }
-  freshId k = FreshIdT $ do { x <- get; put (x + 1); return $ Id k x }
-
-
-runFreshIdT :: Monad m => Int -> FreshIdT m a -> m a
-runFreshIdT x (FreshIdT y) = evalStateT y x
-
-
+idKind :: Id  -> IdKinds
+idKind (NameId x) = nameKind x
+idKind (Id x _) = x
 
 -- ---------------------------------------------------------------------------
 
