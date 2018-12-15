8f34f04eade1ed28a2fdf25ebc448b870a69caae
Hattori, Hiroki <seagull.kamome@gmail.com>
2018-12-15 03:55:49 +0000

    [master] :recycle: cleaning

--- app/Util/Relation.hs
+++ app/Util/Relation.hs
@@ -1,84 +1,148 @@
+module Util.Relation (
+  Relation, fromRelation,
+  fromList, toList,
+  empty, singleton, prune,
+  union, unions,
+  difference, intersection,
+  domain, range,
+  restrictDomain, restrictDomainS, restrictDomainSet,
+  restrictRange,
+  mapDomain, mapRange,
+  partitionDomain,
+  toRelationList
+  ) where
 
--- | extend Data.Set with relation operations
-
-module Util.Relation where
-
-import Data.Monoid
-import Data.Set as Set hiding(map)
-import Util.SetLike
+import Data.List (foldl')
+import Data.Set (Set)
 import qualified Data.Set as Set
+import Data.Map (Map)
 import qualified Data.Map as Map
 
-newtype Rel a b = Rel (Map.Map a (Set b))
-    deriving(Eq)
+import GHC.Exts (IsList(..))
 
-instance (Ord a, Ord b) => Semigroup (Rel a b) where
-    (Rel r1) <> (Rel r2) = Rel $ Map.unionWith Set.union r1 r2
+-- --------------------------------------------------------------------------
 
-instance (Ord a,Ord b) => Monoid (Rel a b) where
-    mempty = Rel mempty
-    -- mappend (Rel r1) (Rel r2) = Rel $ Map.unionWith Set.union r1 r2
+newtype Relation a b = Relation { fromRelation :: Map a (Set b) }
+  deriving (Eq)
 
-instance (Ord a,Ord b) => Unionize (Rel a b) where
-    difference (Rel r1) (Rel r2) = Rel $ Map.differenceWith f r1 r2 where
-        f r1 r2 = if Set.null rs then Nothing else Just rs where
-            rs = Set.difference r1 r2
-    intersection (Rel r1) (Rel r2) = prune $ Map.intersectionWith Set.intersection r1 r2
-
-instance (Ord a,Ord b) => Collection (Rel a b) where
-    fromList xs = Rel $ Map.fromListWith Set.union [ (x,Set.singleton y) | (x,y) <- xs ]
-    toList (Rel r) = [ (x,y) | (x,ys) <- Map.toList r, y <- Set.toList ys]
-
-prune r = Rel $ Map.mapMaybe f r where
-    f s = if Set.null s then Nothing else Just s
+instance (Ord a, Ord b) => Semigroup (Relation a b) where
+  (Relation r1) <> (Relation r2) = Relation $ Map.unionWith Set.union r1 r2
+  {-# INLINE (<>) #-}
+instance (Ord a, Ord b) => Monoid (Relation a b) where
+  mempty = Relation mempty
+  {-# INLINE mempty #-}
+instance (Ord a, Ord b) => IsList (Relation a b) where
+  type Item (Relation a b) = (a, b)
+  fromList xs = Relation $ Map.fromListWith Set.union [ (x, Set.singleton y) | (x, y) <- xs]
+  {-# INLINE fromList #-}
+  toList (Relation r) = [ (x, y) | (x, ys) <- Map.toList r, y <- Set.toList ys]
+  {-# INLINE toList #-}
 
 
-type instance Elem (Rel a b) = (a,b)
-type instance Key (Rel a b) = (a,b)
+
+-- --------------------------------------------------------------------------
+-- Utilities
 
 
-domain :: (Ord a,Ord b) => Rel a b -> Set a
-domain (Rel r) = Map.keysSet r
+-- --------------------------------------------------------------------------
+-- Construction
 
-range :: (Ord a,Ord b) => Rel a b -> Set b
-range (Rel r) = Set.unions $ Map.elems r
+empty :: (Ord a, Ord b) => Relation a b
+empty = mempty
+{-# INLINE empty #-}
 
---flipRelation :: (Ord a, Ord b) => Rel a b -> Rel b a
---flipRelation (Rel r) = Rel $ Set.map (\ (x,y) -> (y,x)) r
 
-restrictDomain :: (Ord a, Ord b) => (a -> Bool) -> Rel a b -> Rel a b
-restrictDomain f (Rel r) = Rel $ Map.filterWithKey (\k _ -> f k) r
+singleton :: (Ord a, Ord b) => (a, b) -> Relation a b
+singleton (x, y) = Relation $ Map.singleton x (Set.singleton y)
+{-# INLINE singleton #-}
 
-restrictDomainS :: (Ord a, Ord b) => a -> Rel a b -> Rel a b
-restrictDomainS x (Rel r) = case Map.lookup x r of
-    Nothing -> Rel mempty
-    Just v -> Rel $ Map.singleton x v
 
-restrictDomainSet :: (Ord a, Ord b) => Set a -> Rel a b -> Rel a b
-restrictDomainSet s (Rel r) = Rel $ Map.filterWithKey (\k _ -> k `Set.member` s) r
+prune :: (Ord a, Ord b) => Map a (Set b) -> Relation a b
+prune r = Relation $ Map.mapMaybe f r
+  where f s = if Set.null s then Nothing else Just s
+{-# INLINE prune #-}
 
-restrictRange :: (Ord a, Ord b) => (b -> Bool) -> Rel a b -> Rel a b
-restrictRange f (Rel r) = Rel $ Map.mapMaybe g r where
-    g s = if Set.null ss then Nothing else Just ss where
-        ss = Set.filter f s
 
-mapDomain :: (Ord a, Ord b, Ord c) => (a -> c) -> Rel a b -> Rel c b
-mapDomain f (Rel r) = Rel $ Map.mapKeys f r
+-- --------------------------------------------------------------------------
+-- Union
 
-mapRange :: (Ord a, Ord b, Ord c) => (b -> c) -> Rel a b -> Rel a c
-mapRange f (Rel r) = Rel $ Map.map (Set.map f) r
+union :: (Ord a, Ord b) => Relation a b ->Relation a b -> Relation a b
+union x y = x <> y
+{-# INLINE union #-}
 
-partitionDomain f (Rel r) = case Map.partitionWithKey (\k _ -> f k) r of
-    (x,y) -> (Rel x,Rel y)
+unions :: (Ord a, Ord b) => [Relation a b] -> Relation a b
+unions = foldl' union empty
+{-# INLINE unions #-}
 
---partitionRange f (Rel r) = Rel $ Set.partition (f . snd) r
 
---applyRelation :: (Ord a, Ord b) => Rel a b -> a -> [b]
---applyRelation r a = Prelude.map snd (Set.toList . unRel $ restrictDomain (== a) r)
+-- --------------------------------------------------------------------------
+-- Difference
 
-unRel (Rel r) = r
+difference :: (Ord a, Ord b) => Relation a b -> Relation a b -> Relation a b
+difference (Relation r1) (Relation r2) = Relation $ Map.differenceWith f r1 r2
+  where f s1 s2 = let s = Set.difference s1 s2
+                   in if Set.null s then Nothing else Just s
+{-# INLINE difference #-}
 
-toRelationList :: (Ord a, Ord b) => Rel a b -> [(a,[b])]
-toRelationList (Rel r) = Map.toList (Map.map Set.toList r)
---toRelationList :: (Ord a, Ord b) => Rel a b -> [(a,[b])]
---toRelationList rel = [ (x, applyRelation rel x) | x <- Set.toList (domain rel)]
+-- --------------------------------------------------------------------------
+-- Intersection
+
+intersection :: (Ord a, Ord b) => Relation a b -> Relation a b -> Relation a b
+intersection (Relation r1) (Relation r2) =
+  prune $ Map.intersectionWith Set.intersection r1 r2
+{-# INLINE intersection #-}
+
+
+-- --------------------------------------------------------------------------
+
+domain :: (Ord a, Ord b) => Relation a b -> Set a
+domain = Map.keysSet . fromRelation
+{-# INLINE domain #-}
+
+range :: (Ord a, Ord b) => Relation a b -> Set b
+range = Set.unions . Map.elems . fromRelation
+{-# INLINE range #-}
+
+
+
+restrictDomain :: (Ord a, Ord b) => (a -> Bool) -> Relation a b -> Relation a b
+restrictDomain f = Relation . Map.filterWithKey (\k _ -> f k) . fromRelation
+{-# INLINE restrictDomain #-}
+
+restrictDomainS :: (Ord a, Ord b) => a -> Relation a b -> Relation a b
+restrictDomainS x = Relation . maybe Map.empty (Map.singleton x) . Map.lookup x . fromRelation
+{-# INLINE restrictDomainS #-}
+
+restrictDomainSet :: (Ord a, Ord b) => Set a -> Relation a b -> Relation a b
+restrictDomainSet s = Relation . Map.filterWithKey (\k _ -> Set.member k s) . fromRelation
+{-# INLINE restrictDomainSet #-}
+
+
+restrictRange :: (Ord a, Ord b) => (b -> Bool) -> Relation a b -> Relation a b
+restrictRange f (Relation r) = Relation $ Map.mapMaybe g r
+  where g s = let ss = Set.filter f s
+               in if Set.null ss then Nothing else Just ss
+{-# INLINE restrictRange #-}
+
+mapDomain :: (Ord a, Ord b, Ord c) => (a -> c) -> Relation a b -> Relation c b
+mapDomain f = Relation . Map.mapKeys f . fromRelation
+{-# INLINE mapDomain #-}
+
+mapRange :: (Ord a, Ord b, Ord c) => (b -> c) -> Relation a b -> Relation a c
+mapRange f = Relation . Map.map (Set.map f) . fromRelation
+{-# INLINE mapRange #-}
+
+partitionDomain :: (Ord a, Ord b)
+                => (a -> Bool) -> Relation a b -> (Relation a b, Relation a b)
+partitionDomain f (Relation r) = (Relation x, Relation y)
+  where (x, y) = Map.partitionWithKey (\k _ -> f k) r
+{-# INLINE partitionDomain #-}
+
+
+toRelationList :: (Ord a, Ord b) => Relation a b -> [(a, [b])]
+toRelationList = Map.toList . Map.map Set.toList . fromRelation
+{-# INLINE toRelationList #-}
+
+
+-- vim: ts=8 sw=2 expandtab :
+
