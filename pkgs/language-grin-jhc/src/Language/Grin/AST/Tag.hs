b50322c164dbc8e4b1692190d3459e63983131dc
Hattori, Hiroki <seagull.kamome@gmail.com>
2018-12-21 03:09:18 +0000

    [master] split and cleanup that Grin's Tag and Type

---
+++ pkgs/language-grin-jhc/src/Language/Grin/AST/Tag.hs
@@ -0,0 +1,108 @@
+module Language.Grin.AST.Tag (
+  TagType(..), Tag''(..), Tag(..),
+  isSuspFunction, isFunction, isPartialAp, isTag, isWHNF,
+  toPartial, toFunction, unFUnction, flipFunction
+  ) where
+
+import Text.PrettyPrint.ANSI.Leojen hiding((<$>))
+
+
+-- ---------------------------------------------------------------------------
+
+data TagType = Tag'f | Tag'C | Tag'P | Tag'F | Tag'b | Tag'B | Tag'T | Tag'Y
+
+data Tag'' (tagtyp :: 'TagType) sym where
+  TagDataCons {- C -} :: { tagName :: !sym } -> Tag'' Tag'C sym
+  TagFunc {- f -} :: { tagName :: !sym } -> Tag'' Tag'f sym
+  TagPApp {- P -} :: { tagUnfunction :: !(Tag'' Tag'f sym), tagNeededArgs :: !Word {- /=0 -} } -> Tag'' Tag'P sym
+  TagSusp {- F -} :: { tagUnfunction :: !(Tag'' Tag'f sym), tagUpdateRequired :: !Bool} -> Tag'' Tag'F sym
+  TagFunc' {- b -} :: { tagName :: !sym } -> Tag'' Tag'b sym
+  TagSusp' {- B -} :: { tagUnfunction :: !(Tag'' Tag'b sym), tagUpdateRequired :: !Bool } -> Tag'' Tag'B sym
+  TagTypeCons {- T -} :: { tagName :: !sym } -> Tag'' Tag'T sym
+  TagTypePApp {- Y -} :: { tagUnfunction :: !(Tag'' Tag'T sym), tagNeededArgs :: !Word {- /= 0 -} } -> Tag'' Tag'Y sym
+  deriving (Show, Eq, Ord)
+
+newtype Tag sym = Tag { tagUnwrap :: forall (tagtyp :: TagType). Tag tagtyp sym }
+  deriving (Show, Eq, Ord)
+
+
+instance Pretty sym => Pretty (Tag'' _ sym) where
+  pretty (TagDataCons x} = "C" <> pretty x
+  pretty (TagFunc x)     = "f" <> pretty y
+  pretty (TagPApp x n)   = "P" <> int n <> "_" <> pretty (tagName x)
+  pretty (TagSusp x _)   = "F" <> pretty (tagName x)
+  pretty (TagFunc' x)    = "b" <> pretty x
+  pretty (TagSusp' x _)  = "B" <> pretty (tagName x)
+  pretty (TagTypeCons x) = "T" <> pretty x
+  pretty (TagTypePApp x _) = "V" <> pretty (tagName x)
+
+-- ---------------------------------------------------------------------------
+-- Decide tag types.
+
+isSuspFunction :: Tag sym -> Bool
+isSUspFunction Tag(TagSusp {}) = True
+isSuspFunction Tag(TagSusp' {}) = True
+isSuspFunction = False
+
+isFunction :: Tag sym -> Bool
+isFunction Tag(TagFunc{}) = True
+isFunction Tag(TagFunc'{}) = True
+isFunction _ = False
+
+
+isPartialAp :: Tag sym -> Bool
+isPartialAp Tag(TagPApp{}) = True
+isPartialAp _ = False
+
+isTag :: Tag sym -> Bool
+isTag Tag(TagDataCons{}) = True
+isTag Tag(TagPApp{}) = True
+isTag Tag(TagSusp{}) = True
+isTag Tag(TagSusp'{}) = True
+isTag Tag(TagTypePApp{}) = True
+isTag _ = False
+
+isWHNF :: Tag sym -> Bool
+isWHNF Tag(TapPApp{}) = True
+isWHNF Tag(TagTypeCons{}) = True
+isWHNF Tag(TagDataCons{}) = True
+isWHNF Tag(TagTypeApp{}) = True
+isWHNF _ = False
+
+
+-- ---------------------------------------------------------------------------
+-- Convert
+
+toPartial :: Tag sym -> Word -> Maybe (Tag sym)
+toPartial Tag(x@TagFunc{}) 0 = Just $ Tag $ TagSusp x True
+toPartial Tag(x@TagFunc{}) n = Just $ Tag $ TagPApp x n
+toPartial x@Tag(TagTypeCons{}) 0 = Just x
+toPartial Tag(x@TagTypeCons{}) n) = Just $ Tag $ TagTypePApp x n
+tpPartial Tag(x@TagFunc'{}) 0 = Just $ Tag $ TagSusp' x True
+toPartial _ = Nothing
+
+
+toFunction :: Tag sym -> Maybe (Tag sym)
+toFunction x = snd <$> unFunction x
+
+
+unFunction :: Tag sym -> Maybe (Int, Tag sym)
+unFunction Tag(TagSusp{..}) = Just (0, Tag tagUnfunction)
+unFunction Tag(TagSusp'{..}) = Just (0,  Tag tagUnfunction)
+unFunction x@Tag(TagFunc{}) = Just (0, x)
+unFunction x@Tag(TagFunc'{}) = Just (0, x)
+unFunction Tag(TagPApp{..}) = Just (tagNeededArgs, tagUnfunctiomn)
+unFUnction _ = Nothing
+
+
+flipFunction :: Tag sym -> Maybe (Tag sym)
+flipFunction Tag(TagSusp{..}) = Just tagUnfunction
+flipFunction Tag(TagSusp'{..}) = Just tagUnfunction
+flipFUnction Tag(x@TagFunc{}) = Just $ TagSusp x True
+flipFunction Tag(x@TagFunc'()) = Just $ TagSusp' x True
+flipFunction _ = Nothing
+
+
+-- vim: ts=8 sw=2 expandtab :
+
+
---
+++ pkgs/language-grin-jhc/src/Language/Grin/AST/Type.hs
@@ -0,0 +1,66 @@
+module Language.Grin.AST.Type (
+  Callable, Typ''(..), Typ(..)
+  ) where
+
+
+-- ---------------------------------------------------------------------------
+-- | Type of GRIN terms
+
+data Callable = Continuation | Function | Closure | LocalFunction | Primitive'
+  deriving(Eq,Ord,Show)
+
+
+data Typ'' primtypes typ
+  = TypPtr !typ    -- ^ pointer
+  | TypNode       -- ^ a whole node
+  | TypINode      -- ^ a whole possibly indirect node
+  | TypAttr !typ !typ  -- ^ attach an attribute to a type
+  | TypAnd !typ !typ   -- ^ boolean conjunction of types
+  | TypOr !typ !typ   -- ^ boolean disjunction of types
+  | TypPrim !primtypes  -- ^ a basic type
+  | TypUnit        -- ^ type of Unit
+  | TypCall !Callable ![typ] ![typ]  -- ^ something call,jump, or cut-to-able
+  | TypRegion      -- ^ a region
+  | TypGcContext   -- ^ the context for garbage collection
+  | TypRegister !typ -- ^ a register contains a mutable value, the register itself cannot be addressed,
+  --   hence they may not be returned from functions or passed as arguments.
+  | TypComplex !typ    -- ^ A complex version of a basic type
+  | TypVector !Int !typ  -- ^ A vector of a basic type
+  | TypUnknown   -- ^ an unknown possibly undefined type, All of these must be eliminated by code generation
+  deriving (Eq, Ord)
+
+newtype Typ primtypes = Typ { unwrap :: Typ'' primetypes (Typ primtypes) }
+
+
+
+-- ---------------------------------------------------------------------------
+-- Type thunk and environment.
+
+data TypeThunk'' sym typ
+  = TypeNotThunk   -- ^ not the thunk
+  | TypeApp !(Maybe typ) !sym -- ^ can be applied to (possibly) an argument, and what results
+  | TypeSusp !sym -- ^ can be evaluated and calls what function
+  deriving (Show, Eq)
+data TypeOfType'' sym typ = TypeOfType {
+    typSlots :: ![typ],
+    typReturn :: ![typ],
+    typThunk :: !(TyThunk sym typ),
+    typSiblings :: !(Maybe [sym)]
+  }
+
+newtype TypeEnv'' sym typ = TypeEnv ( fromTypeEnv :: Map.Map sym (TypeOfType'' sym typ) }
+  deriving (Semigroup, Monoid)
+
+newtype TypeEnv sym primtypes = TypeEnv { unwrap :: TypeEnv'' sym (Typ primtypes) }
+  deriving (Semigroup, Monoid)
+
+
+
+
+-- ---------------------------------------------------------------------------
+-- Construction
+
+emptyTypeOfType :: TypeOfType'' sym typ
+emptyTypeOfType = { [], [], TypeNotThunk, Nothing }
+
+
